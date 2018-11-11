5aafae59f151dc7987506260309d3d42e3bab488
Kelnerowski Florian <florian@raspberrypi>
2018-11-11 14:04:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# python-temperature
+
+Python script writing every hour the temperature of the computer 
+
+The generated file (data.json) is used to create graphs [https://github.com/Nairolf92/raspberry-temperature]
+
+The generated data looks like this :
+
+```
+{"01/11/2018 11:34:50": "36.5", "01/11/2018 12:35:00": "32.7", "01/11/2018 13:35:00": "33.2", "01/11/2018 14:35:00": "33.2", "04/11/2018 11:13:29": "33.8", "04/11/2018 12:59:52": "34.2" } 
+```
---
+++ temperature.py
@@ -0,0 +1,50 @@
+
+import os
+import time
+import json
+import datetime
+from shutil import copyfile
+
+def getTemp():
+	temp = os.popen("vcgencmd measure_temp").readline()
+	temp = temp.replace("temp=", "")
+	temp = temp[:4]
+	return temp
+
+def writeToJSONFile(key,value):
+	with open('data.json') as json_file :
+		json_decoded = json.load(json_file)
+
+	json_decoded[key] = value
+
+	with open('data.json', 'w') as json_file :
+		json.dump(json_decoded, json_file, sort_keys=True)
+
+def getDate():
+	date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+	return date
+
+# Uncomment the copy() function if necessary
+def writeCopyAndSleep():
+	while True:
+		writeToJSONFile(getDate(),getTemp())
+#		copy()
+		time.sleep(3600)
+
+def createJSON():
+	data = {}
+	with open('data.json', 'w') as json_file :
+		json.dump(data, json_file)
+
+def copy() :
+	copyfile('data.json','/var/www/html/raspberry-temperature/src/data.json')
+
+if os.path.isfile('data.json'):
+	print 'file found'
+	writeCopyAndSleep()
+else :
+	print 'file not found'
+	createJSON()
+	writeCopyAndSleep()
+
+
